9a33c42955740f58d3e691bf8896d5f68c3ddd67
williamcostaramos <wulliamcostaramos@gmail.com>
2021-03-30 22:27:48 -0300

    fix: Produces para MediaType.Json

--- src/main/java/br/com/williamramos/apipagamento/controller/PagamentoController.java
+++ src/main/java/br/com/williamramos/apipagamento/controller/PagamentoController.java
@@ -3,6 +3,7 @@ package br.com.williamramos.apipagamento.controller;
 import br.com.williamramos.apipagamento.domain.model.Pagamento;
 import br.com.williamramos.apipagamento.domain.repository.PagamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pagamentos")
+@RequestMapping(path = "/pagamentos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PagamentoController {
 
     @Autowired
@@ -25,6 +26,7 @@ public class PagamentoController {
     public ResponseEntity<Pagamento> findById(@PathVariable("id") Long id) {
         return repository.findById(id).map(item -> ResponseEntity.ok().body(item)).orElse(ResponseEntity.notFound().build());
     }
+
     @GetMapping("/cpf/{cpf}")
     public List<Pagamento> findByCpf(@PathVariable("cpf") String cpf) {
         return repository.findByCpf(cpf);
